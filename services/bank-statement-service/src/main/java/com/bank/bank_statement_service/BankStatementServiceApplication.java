05b15ea0e40b20c7ff7656cf7ab25a24b6d34fff
Ramon Lorente <ramon_lorente@yahoo.com>
2025-03-01 13:38:14 -0500

    adding missing files.

---
+++ services/bank-statement-service/src/main/java/com/bank/bank_statement_service/BankStatementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bank.bank_statement_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankStatementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankStatementServiceApplication.class, args);
+	}
+
+}
